317b23f0988cfeae58290cbf23a3b3c0884e3181
Liuxizi Xu <xlxzbisulyn@gmail.com>
2016-04-10 22:49:49 -0400

    clean up

--- graph/graph.py
+++ graph/graph.py
@@ -21,8 +21,7 @@ def date_range(start_date, end_date, increment, period):
         nxt += delta
     return result
 
-@app.route('/get_data')
-def hello_world():
+def generate_mock_data():
     today = datetime.datetime.now().date()
     ten_am = datetime.time(hour=10)
     eight_pm = datetime.time(hour=20)
@@ -52,47 +51,51 @@ def hello_world():
         "one_minute_interval": [(t.strftime("%H:%M"), one_min_data[t]) for t in minute_intervals]
     }
 
-    return jsonify(ret_val)
-    # return ret_val
+    # return jsonify(ret_val)
+    return ret_val
 
-# @app.route('/graph')
-# def draw_line_graph():
-#     parsed = hello_world()
-#     current_date = parsed['date']
-#     fifteen_min = parsed['fifteen_minute_interval']
-#     imp_temps = [float(i[1]) for i in fifteen_min]
-#     times = [str(i[0]) for i in fifteen_min]
-#     # create a bar chart
-#     title = 'Temps for %s' % current_date.strftime("%Y-%m-%d %H:%M:%S")
-#     bar_chart = pygal.Line(width=1200, height=600,
-#                           explicit_size=True, title=title, x_label_rotation=40)
-#     bar_chart.x_labels = times
-#     bar_chart.add('Sensor1', imp_temps)
-#
-#     fifteen_min2 = hello_world()['fifteen_minute_interval']
-#     fifteen_min3 = hello_world()['fifteen_minute_interval']
-#     fifteen_min4 = hello_world()['fifteen_minute_interval']
-#     fifteen_min5 = hello_world()['fifteen_minute_interval']
-#     imp_temps2 = [float(i[1]) for i in fifteen_min2]
-#     imp_temps3 = [float(i[1]) for i in fifteen_min3]
-#     imp_temps4 = [float(i[1]) for i in fifteen_min4]
-#     imp_temps5 = [float(i[1]) for i in fifteen_min5]
-#
-#     bar_chart.add('Sensor2', imp_temps2)
-#     bar_chart.add('Sensor3', imp_temps3)
-#     bar_chart.add('Sensor4', imp_temps4)
-#
-#     html = """
-#         <html>
-#              <head>
-#                   <title>%s</title>
-#              </head>
-#               <body>
-#                  %s
-#              </body>
-#         </html>
-#         """ % (title, bar_chart.render())
-#     return html
+@app.route('/get_data')
+def get_data():
+    return jsonify(generate_mock_data())
+
+@app.route('/graph')
+def draw_line_graph():
+    parsed = generate_mock_data()
+    current_date = parsed['date']
+    fifteen_min = parsed['fifteen_minute_interval']
+    imp_temps = [float(i[1]) for i in fifteen_min]
+    times = [str(i[0]) for i in fifteen_min]
+    # create a bar chart
+    title = 'Temps for %s' % current_date.strftime("%Y-%m-%d %H:%M:%S")
+    bar_chart = pygal.Line(width=1200, height=600,
+                          explicit_size=True, title=title, x_label_rotation=40)
+    bar_chart.x_labels = times
+    bar_chart.add('Sensor1', imp_temps)
+
+    fifteen_min2 = generate_mock_data()['fifteen_minute_interval']
+    fifteen_min3 = generate_mock_data()['fifteen_minute_interval']
+    fifteen_min4 = generate_mock_data()['fifteen_minute_interval']
+    fifteen_min5 = generate_mock_data()['fifteen_minute_interval']
+    imp_temps2 = [float(i[1]) for i in fifteen_min2]
+    imp_temps3 = [float(i[1]) for i in fifteen_min3]
+    imp_temps4 = [float(i[1]) for i in fifteen_min4]
+    imp_temps5 = [float(i[1]) for i in fifteen_min5]
+
+    bar_chart.add('Sensor2', imp_temps2)
+    bar_chart.add('Sensor3', imp_temps3)
+    bar_chart.add('Sensor4', imp_temps4)
+
+    html = """
+        <html>
+             <head>
+                  <title>%s</title>
+             </head>
+              <body>
+                 %s
+             </body>
+        </html>
+        """ % (title, bar_chart.render())
+    return html
 
 if __name__ == '__main__':
     app.run(debug=True)
--- server/server.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var app = express();
-
-var port = process.env.PORT || 8080;
-
-app.use(express.static(__dirname + '/public'));
-app.listen(port);	
-console.log('App running on port ' + port + '...'); 	// shoutout to the user
