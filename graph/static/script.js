f970440fbcbafafbe05776c18f38add9c286195f
Liuxizi Xu <xlxzbisulyn@gmail.com>
2016-04-11 13:44:54 -0400

    add some js

---
+++ graph/static/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){ 
+    $("form").submit(function(){
+    	alert("switch interval");
+    });
+
+    $('#userUpdate').click(function() {
+		alert("Blanket preference is updated!");
+	});
+});
+
