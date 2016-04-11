74286b501473a9151ce5e026d06210cac37dab43
Liuxizi Xu <xlxzbisulyn@gmail.com>
2016-04-11 15:06:57 -0400

    fix bugs

--- graph/app.py
+++ graph/app.py
@@ -53,36 +53,39 @@ def generate_mock_data():
     # return jsonify(ret_val)
     return ret_val
 
+data_set = []
+for i in range(0, 5):
+    data_set.append(generate_mock_data())
+
+# @app.route('/signUpUser', methods=['POST'])
+# def signUpUser():
+#     user =  request.form['username'];
+#     password = request.form['password'];
+#     return json.dumps({'status':'OK','user':user,'pass':password})
+
 @app.route('/get_data')
 def get_data():
-    return jsonify(generate_mock_data())
+    return jsonify(data_set[0])
 
 @app.route('/graph')
-def draw_line_graph():
-    parsed = generate_mock_data()
-    current_date = parsed['date']
-    fifteen_min = parsed['fifteen_minute_interval']
-    imp_temps = [float(i[1]) for i in fifteen_min]
-    times = [str(i[0]) for i in fifteen_min]
+def draw_line_graph(data_set, interval = 'fifteen_minute_interval'):
+    # get data and time duration
+    current_date = data_set[0]['date']
+    times = [str(i[0]) for i in data_set[0][interval]]
     # create a bar chart
     title = 'Temperature for %s' % current_date.strftime("%Y-%m-%d")
     bar_chart = pygal.Line(width=1200, height=600,
                           explicit_size=True, title=title, x_label_rotation=40, style=CleanStyle)
+    # add time label
     bar_chart.x_labels = times
-    bar_chart.add('Sensor1', imp_temps)
-
-    fifteen_min2 = generate_mock_data()['fifteen_minute_interval']
-    fifteen_min3 = generate_mock_data()['fifteen_minute_interval']
-    fifteen_min4 = generate_mock_data()['fifteen_minute_interval']
-    fifteen_min5 = generate_mock_data()['fifteen_minute_interval']
-    imp_temps2 = [float(i[1]) for i in fifteen_min2]
-    imp_temps3 = [float(i[1]) for i in fifteen_min3]
-    imp_temps4 = [float(i[1]) for i in fifteen_min4]
-    imp_temps5 = [float(i[1]) for i in fifteen_min5]
-
-    bar_chart.add('Sensor2', imp_temps2)
-    bar_chart.add('Sensor3', imp_temps3)
-    bar_chart.add('Sensor4', imp_temps4)
+    # add line plots
+    sensorNum = 1
+    for sensor in data_set:
+        temp_reading = sensor[interval]
+        imp_temps = [float(i[1]) for i in temp_reading]
+        temp_label = 'Sensor ' + str(sensorNum)
+        bar_chart.add(temp_label, imp_temps)
+        sensorNum += 1
 
     # html = """
     #     <html>
@@ -101,7 +104,7 @@ def draw_line_graph():
 
 @app.route('/')
 def main():
-    return render_template('index.html', graph = draw_line_graph())
+    return render_template('index.html', graph = draw_line_graph(data_set))
 
 if __name__ == '__main__':
     app.run(debug=True)
--- graph/static/script.js
+++ graph/static/script.js
@@ -1,10 +1,25 @@
 $(document).ready(function(){ 
-    $("form").submit(function(){
-    	alert("switch interval");
-    });
+	// switch interval
+    $('#switchInterval').click(function() {
 
+    })
+    // user update preference
     $('#userUpdate').click(function() {
-		alert("Blanket preference is updated!");
+    	var temp = $('#userTemperature').val();
+        var time = $('#userTime').val();
+        console.log(temp + " "+ time);
+
+        $.ajax({
+            url: '/updateUser',
+            data: $('form').serialize(),
+            type: 'POST',
+            success: function(response) {
+                console.log(response);
+            },
+            error: function(error) {
+                console.log(error);
+            }
+        });
 	});
 });
 
